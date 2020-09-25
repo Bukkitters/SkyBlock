c31dbbfbc98ba0c73f5500c2abf6e913d9dd9517
JustSoyer <soyeraccount@gmail.com>
2020-09-25 20:35:47 +0600

    Main class edited

--- src/org/Bukkitters/SkyBlock/Main.java
+++ src/org/Bukkitters/SkyBlock/Main.java
@@ -1,19 +1,21 @@
 package org.Bukkitters.SkyBlock;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
 	public void onEnable() {
-		
+		send("&aPlugin enabled!");
 	}
 	
 	public void onDisable() {
-		
+		send("&cPlugin disabled!");
 	}
 	
 	public void send(String s) {
-		
+		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "[SkyBlock] " + s));
 	}
 	
 }
---
+++ src/org/Bukkitters/SkyBlock/Utils/ChatColors.java
@@ -0,0 +1,11 @@
+package org.Bukkitters.SkyBlock.Utils;
+
+import org.bukkit.ChatColor;
+
+public class ChatColors {
+
+	public String color(String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
+	}
+
+}
